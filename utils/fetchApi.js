caa667e9411908f0f1d0d411dd5258b95fef0b86
waweru-steve <wawerusteve142@yahoo.com>
2021-12-18 21:22:11 +0300

    Initialized base url for the api connection

---
+++ utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get((url), {
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': '117514e98amshcdf73eab801b4b6p1cde8cjsnbb7eb92b4316'
+    }
+
+  })
+}
